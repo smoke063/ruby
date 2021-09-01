3df4c698f17e4f5e3251b7a1e8e33c9184151a6a
danila <danila.kononov@netcracker.com>
2021-09-01 19:29:15 +0400

    Home work lesson-4

---
+++ main.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+pi = 3.1415926535
+
+puts pi.floor 2
+
+days = %i[понедельник вторник среда четверг пятница суббота воскресенье]
+
+days.each { |day| puts day }
+
+colors = { green: 'зеленый', red: 'красный', blue: 'синий', pink: 'розовый' }
+
+colors.each { |color| puts color }
+
+puts 'Hello'.reverse
+
+puts '  hello world   '.strip
+
+h = 'hello'
+w = 'world'
+
+puts "#{h} #{w}"
+
+
